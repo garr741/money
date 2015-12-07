894647672fe5412e79ed3125d178bc758c439249
Tylor Garrett <tylorgarrett@gmail.com>
2015-12-06 23:29:12 -0500

    Added a working graph to the CIC page

--- CIC/index.js
+++ CIC/index.js
@@ -1,13 +1,70 @@
 var app = angular.module('CompoundInterestCalculator', ['chart.js']);
 
-app.controller('CICController', ['$scope', function($scope) {
+app.controller('CICController', ['$scope', '$filter', function($scope, $filter) {
     $scope.title = "Compound Interest Calculator";
-    $scope.FV = function(P, r, y, c) {
-        r = (r / 100);
-        return P * (Math.pow((1 + r), y)) + c * ((Math.pow((1 + r), y + 1) - (1 + r)) / r);
+    $scope.FV = function(P, r, y,c){
+        return futureValue(P,r,y,c);
     }
 
-    $scope.labels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-    $scope.data = [30, 50, 10];
+    var futureValue = function(P, r, y, c) {
+        r = (r / 100);
+        var result = P * (Math.pow((1 + r), y)) + c * ((Math.pow((1 + r), y + 1) - (1 + r)) / r);
+        //return $filter('number')(result, 2);
+        return Math.floor(result);
+    }
+
+    $scope.YearList = [0,1,2,3,4];
+    $scope.AmountInvestedList = [0];
+    $scope.TotalAmountList = [0];
+
+    $scope.labels = $scope.YearList;
+    $scope.series = ['Amount Invested', 'Total Amount'];
+    $scope.data = [$scope.AmountInvestedList, $scope.TotalAmountList]
+
+    $scope.onClick = function(points, evt) {
+        console.log(points, evt);
+    };
+
+    var listOfTotalAmountInvested = function(){
+        var results = [];
+        for (var i = 0; i < $scope.y; i++) {
+            results.push($scope.c * i);
+        };
+        return results;
+    }
+
+    var listOfYears = function(){
+        var results = [];
+        var a = $scope.y;
+        for  (var i = 1; i<=a; i++){
+            results.push(i)
+        }
+        return results;
+    }
+
+    var listOfTotalAmount = function(){
+        var results = [];
+        for (var i=0; i<$scope.y; i++){
+            results.push(futureValue($scope.P, $scope.r, i, $scope.c))
+        }
+        return results;
+    }
+
+    $scope.$watch('r', function(newValue, oldValue) {
+        updateGraphInfo()
+    });
+
+    $scope.$watch('y', function(newValue, oldValue) {
+        updateGraphInfo()
+    });
+
+    $scope.$watch('c', function(newValue, oldValue) {
+        updateGraphInfo()
+    });
+
+    var updateGraphInfo = function(){
+        $scope.data = [listOfTotalAmountInvested(), listOfTotalAmount()];
+        $scope.labels = listOfYears();
+    }
 
 }]);
